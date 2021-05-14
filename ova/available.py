d613d8a2223a19ee157491cabf8b43b539d37f5d
Barry Li <bli031@uottawa.ca>
2021-05-14 05:28:46 +0000

    Added logging.

--- ova/available.py
+++ ova/available.py
@@ -2,9 +2,12 @@
 
 from datetime import date, timedelta
 import json
+import logging
 
 import requests
 import pkg_resources
+
+log = logging.getLogger(__name__)
 
 LOCATIONS = pkg_resources.resource_filename('ova', 'locations.json')
 
@@ -12,8 +15,10 @@ with open(LOCATIONS) as f:
     locations = json.load(f)
 
 
-def get_next_available(location=locations['Ottawa City Hall'], dose=1):
-    url = f'https://api.covaxonbooking.ca/public/locations/{location}/availability'
+def get_next_available(location='Ottawa City Hall', dose=1):
+    location_id = locations[location]
+    
+    url = f'https://api.covaxonbooking.ca/public/locations/{location_id}/availability'
 
     headers = {'content-type': 'application/json;charset=UTF-8'}
 
@@ -25,16 +30,18 @@ def get_next_available(location=locations['Ottawa City Hall'], dose=1):
         'url': 'https://vaccine.covaxonbooking.ca/manage/appointment-select'
     }
 
-    response = requests.post(url, headers=headers, data=json.dumps(data))
-
-    response_json = response.json()
+    try:
+        log.info(f'Fetching available dates for: {location}.')
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        response_json = response.json()
+    except Exception as e:
+        log.error(f'Failed to fetch availabe dates for: {location}. Error: {e}.')
 
     for avail in response_json['availability']:
         if avail['available']:
-            return avail['date']
-    return 'No available slot in the next 30 days.'
+            return {'location': location, 'next_available': avail['date']}
 
 
 if __name__ == "__main__":
     next_avail = get_next_available()
-    print(next_avail)
+    log.info(next_avail)
